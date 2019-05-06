e531a31f65ee3b09af4a97780cbd51712f7f6334
Administrator <Administrator@LZ73HWF8T2ULSMH>
2019-05-06 09:30:14 +0800

    ok

--- sdsd/src/main/java/sdsd/bb.java
+++ sdsd/src/main/java/sdsd/bb.java
@@ -3,8 +3,13 @@ package sdsd;
 public class bb {
 
 	public static void main(String[] args) {
+		System.out.println("bb.main()");
+		System.out.println(0);
+		System.out.println("aaaa");
 		// TODO Auto-generated method stub
-
+		int a =10;
+		int b = 520;
+		// TODO Auto-generated method stub
 	}
 
 }
